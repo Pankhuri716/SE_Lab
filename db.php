d6729960750eb23991404cafd9ce1ddb8864340e
Pankhuri716 <pankhuri716@gmail.com>
2026-01-28 12:02:10 +0530

    Delete db.php

--- db.php
+++
@@ -1,8 +0,0 @@
-<?php
-// db.php
-include_once "config.php";
-
-$conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-if (!$conn) {
-    die("Database connection failed: " . mysqli_connect_error());
-}
